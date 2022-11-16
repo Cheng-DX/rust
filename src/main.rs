328fad46054777fd09cd1ff252648825394aef30
Cheng-DX <2337040896@qq.com>
2022-11-16 22:08:38 +0800

    test: test all modules in main

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,30 @@
+mod functional;
 mod heap;
 mod lifecycle;
 mod option;
 mod traits;
+mod utils;
 
+use functional::{closure, iterator};
 use heap::{map, vector};
 use lifecycle::{advanced, basic, struct_in};
 use option::file;
 use traits::post;
+use utils::print_utils;
 
 fn main() {
-    run_tests(vec![
-        post::test,
-        map::test,
-        vector::test,
-        file::test,
-        basic::test,
-        struct_in::test,
-        advanced::test,
-    ]);
-}
-
-fn run_tests(fns: Vec<fn() -> ()>) {
-    for f in fns {
-        println!("{}", "=".repeat(60));
-        f();
-    }
+    print_utils::run_tests(
+        vec![
+            post::test,
+            map::test,
+            vector::test,
+            file::test,
+            basic::test,
+            struct_in::test,
+            advanced::test,
+            closure::test,
+            iterator::test,
+        ],
+        "=",
+    );
 }
