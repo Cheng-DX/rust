0eb05762368d7e534b9bbcbccc3ed806cd3ac438
Cheng-DX <2337040896@qq.com>
2022-11-19 21:05:00 +0800

    chore: thread part 3

--- src/thread.rs
+++ src/thread.rs
@@ -118,7 +118,23 @@ pub mod use_thread {
 
     fn mutex() {
         let pair = Arc::new((Mutex::new(false), Condvar::new()));
-        let pairs = pair.clone();
+        let pair2 = pair.clone();
+
+        thread::spawn(move || {
+            let &(ref lock, ref cvar) = &*pair2;
+            let mut started = lock.lock().unwrap();
+            println!("changing started");
+            *started = true;
+            cvar.notify_one();
+        });
+
+        let &(ref lock, ref cvar) = &*pair;
+        let mut started = lock.lock().unwrap();
+        while !*started {
+            started = cvar.wait(started).unwrap();
+        }
+
+        println!("started changed");
     }
 
     pub fn test() {
